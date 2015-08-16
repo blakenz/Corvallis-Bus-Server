6788af7610c655db608852c281cdcb98fb7ac1ab
Rikki Gibson <rikkigibson@gmail.com>
2015-08-16 13:43:30 -0700

    trying and failing to unit test

---
+++ CorvallisBusAPITest/TransitManagerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CorvallisBusAPITest
+{
+    public class TransitManagerTest
+    {
+        [Fact]
+        public void IsXUnitGoodTest()
+        {
+            Assert.Equal(actual: false, expected: true);
+        }
+    }
+}
